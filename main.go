fe9170cc78bacfbf6ae3efbe6f70b66c49a0035b
Peggy Li <peggy.li@clever.com>
2016-10-30 14:40:37 -0700

    Update and cleanup

---
+++ main.go
@@ -0,0 +1,18 @@
+package web
+
+import (
+	"net/http"
+	"strings"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// TODO: handle favicon properly
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+	http.ServeFile(w, r, "index.html")
+}
